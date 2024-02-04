aff232fa657369d57b74a74b79ed97980be5276b
marely <marely.carcamo@gmail.com>
2024-02-04 10:05:45 -0300

    corrección preventDefault()

--- js/validaRut.js
+++ js/validaRut.js
@@ -1,34 +1,42 @@
+// PROGRAMA VALIDACION DE RUT CHILENO
+
+// evento click botón validar
+document.getElementById("bt-validar").addEventListener("click", function(event) {
+    event.preventDefault();
+    var inputRut = document.getElementById('input-rut').value;
+    // Primero, verifica si el RUT tiene el formato correcto
+   if (!validarFormatoRut(inputRut)) { // Pasa el valor del RUT a la función
+    document.getElementById('resultado').innerText = "El formato del RUT no es válido.";
+    }else{
+        validarRut(inputRut);
+    }
+});
+
+
 // La función "validarFormatoRut" verifica si el RUT ingresado tiene el formato correcto.
-function validarFormatoRut(rut) {
-    return /^\d{1,8}-[\dKk]$/.test(rut);
+function validarFormatoRut(inputRut) {
+    return /^\d{1,8}-[\dKk]$/.test(inputRut);
 }
 
+
 // La función "validarRut" es responsable de validar el número de identificación nacional chileno (RUT).
-function validarRut() {
-    var rutCompleto = document.getElementById('rut').value;
-
-    // Primero, verifica si el RUT tiene el formato correcto
-    if (!validarFormatoRut(rutCompleto)) {
-        document.getElementById('resultado').innerText = "El formato del RUT no es válido.";
-        return;
-    }
-
-    var partes = rutCompleto.split('-');
-    var texto = partes[0];
+    function validarRut(inputRut) {
+    var partes = inputRut.split('-');
+    var rut = partes[0];
     var dvRut = partes[1];
-
-    var pos = texto.length;
+    var pos = rut.length;
     var suma = 0;
-
+    
     while (pos > 0) {
         for (var i = 2; i < 8; i++) {
             pos -= 1;
-            suma += i * parseInt(texto.charAt(pos));
+            suma += i * parseInt(rut.charAt(pos));
             if (pos == 0) {
                 break;
             }
         }
     }
+
     var d = 11 - (suma % 11);
     var dv = (d === 10) ? "K" : ((d === 11) ? "0" : String(d));
     var esValido = dv.toUpperCase() === dvRut.toUpperCase();
@@ -40,8 +48,11 @@ function validarRut() {
     elementoResultado.innerText = esValido ? "El RUT es válido." : "El RUT no es válido.";
 }
 
-// La función "limpiar" limpia el campo de entrada y el resultado
-function limpiar() {
-    document.getElementById('rut').value = '';
+
+// Evento click botón Limpiar. La función "limpiar" limpia el campo de entrada y el resultado
+document.getElementById("bt-limpiar").addEventListener("click", function(event) {
+    event.preventDefault();
+    document.getElementById('input-rut').value = '';
     document.getElementById('resultado').innerText = '';
-}
+    
+});
