9aa6065445cf6ecb2bea7049589005694917fb79
marely <marely.carcamo@gmail.com>
2024-02-04 01:01:33 -0300

    updating

--- validaRut.js
+++ validaRut.js
@@ -1,6 +1,18 @@
+// La función "validarFormatoRut" verifica si el RUT ingresado tiene el formato correcto.
+function validarFormatoRut(rut) {
+    return /^\d{1,8}-[\dKk]$/.test(rut);
+}
+
 // La función "validarRut" es responsable de validar el número de identificación nacional chileno (RUT).
 function validarRut() {
     var rutCompleto = document.getElementById('rut').value;
+
+    // Primero, verifica si el RUT tiene el formato correcto
+    if (!validarFormatoRut(rutCompleto)) {
+        document.getElementById('resultado').innerText = "El formato del RUT no es válido.";
+        return;
+    }
+
     var partes = rutCompleto.split('-');
     var texto = partes[0];
     var dvRut = partes[1];
@@ -23,11 +35,13 @@ function validarRut() {
 
     // Obtén una referencia al elemento donde quieres mostrar el resultado
     var elementoResultado = document.getElementById('resultado');
-
+    alert("dv = " + dv);
     // Actualiza el contenido del elemento con el resultado de la validación
-    if (esValido) {
-        elementoResultado.innerText = "El RUT es válido.";
-    } else {
-        elementoResultado.innerText = "El RUT no es válido.";
-    }
+    elementoResultado.innerText = esValido ? "El RUT es válido." : "El RUT no es válido.";
+}
+
+// La función "limpiar" limpia el campo de entrada y el resultado
+function limpiar() {
+    document.getElementById('rut').value = '';
+    document.getElementById('resultado').innerText = '';
 }
